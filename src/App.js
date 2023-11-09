2a7e2ecf0b2409f295abbcb3a0c9980e61060563
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-11-09 16:22:37 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import './App.css';
 import InputTodo from './components/InputTodo';
 import ListTodo from './components/ListTodo';
 
+
+
 function App() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://todo-server:4000/test');
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  fetchData();
   return (
     <div className="App">
       <InputTodo />
