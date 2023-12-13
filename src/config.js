e5c7de69c218926ec2cc0583edb0a705f332cfa0
sri-karthick-k <srikarthickk.mca22@rvce.edu.in>
2023-12-13 12:05:34 +0530

    modify url to master node (RASPBERRY PI)

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiUrl: "http://192.168.0.105:31001",
+    // apiUrl: "http://192.168.0.105:31001",
+    apiUrl: "http://192.168.100.200:31001"
   };
   
 export default config;
